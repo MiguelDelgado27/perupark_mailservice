93d3e0b1582c2c11cfc51381b32bad62773af80c
Miguel Delgado <Miguel.Delgado@extendhealth.com>
2023-11-15 16:33:01 -0500

    install cors

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const cors = require('cors');
 app.use(cors({
     origin: 'https://migueldelgado27.github.io'
 }));
+
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-    
+
 //let email = require('./mail')
 let email = require('./newMail')
 
-app.listen(port,()=> console.info(`Server has started on port ${port}`));
 app.use(express.static('public'))
 
 app.use(email)
+
+app.listen(port,()=> console.info(`Server has started on port ${port}`));
